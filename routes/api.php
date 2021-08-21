dc8f637e534e1030688027f2ccc1c30c96087b92
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-08-21 12:48:24 -0300

    Register - Category

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\{
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    /* REGISTER
+    ================================================== */
+    Register\Category\CategoryController, ## CATEGORY
+
+}; // Controllers
+
+Route::get ('/', function () {
+    return response()->json( [ 'message' => 'success' ] );
 });
+
+/* CATEGORY
+================================================== */
+Route::apiResource( 'register/category', CategoryController::class ); ## CATEGORY
